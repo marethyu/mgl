3d7a2e1340b8e3cb3b30de1982be87f43d03665a
marethyu <codingexpert123@gmail.com>
2022-06-09 17:27:25 -0700

    Implement 4x4 matrix inverse

--- linalg.h
+++ linalg.h
@@ -556,6 +556,8 @@ namespace mygl
     template<typename T> const SquareMatrix<T, 4> CreateRotationMatrix4(T yaw, T pitch, T roll);
     template<typename T> const SquareMatrix<T, 4> CreateRotationMatrix4(const Quaternion<T>& q); // q is a unit quaternion
 
+    template<typename T> const SquareMatrix<T, 4> Inverse4(const SquareMatrix<T, 4>& m); // experimental
+
     // TODO impl inverse, gaussian elimination, solve systems of eqns, determinant, find eigenvalues and eigenvectors etc... Nice reference: https://ubcmath.github.io/MATH307/index.html
 
     template<typename T, size_t N>
@@ -836,6 +838,63 @@ namespace mygl
         R[3][3] = 1;
 
         return R;
+    }
+
+    // https://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
+    template<typename T>
+    const SquareMatrix<T, 4> Inverse4(const SquareMatrix<T, 4>& m)
+    {
+        SquareMatrix<T, 4> im;
+
+        T A2323 = m[2][2] * m[3][3] - m[2][3] * m[3][2];
+        T A1323 = m[2][1] * m[3][3] - m[2][3] * m[3][1];
+        T A1223 = m[2][1] * m[3][2] - m[2][2] * m[3][1];
+        T A0323 = m[2][0] * m[3][3] - m[2][3] * m[3][0];
+        T A0223 = m[2][0] * m[3][2] - m[2][2] * m[3][0];
+        T A0123 = m[2][0] * m[3][1] - m[2][1] * m[3][0];
+        T A2313 = m[1][2] * m[3][3] - m[1][3] * m[3][2];
+        T A1313 = m[1][1] * m[3][3] - m[1][3] * m[3][1];
+        T A1213 = m[1][1] * m[3][2] - m[1][2] * m[3][1];
+        T A2312 = m[1][2] * m[2][3] - m[1][3] * m[2][2];
+        T A1312 = m[1][1] * m[2][3] - m[1][3] * m[2][1];
+        T A1212 = m[1][1] * m[2][2] - m[1][2] * m[2][1];
+        T A0313 = m[1][0] * m[3][3] - m[1][3] * m[3][0];
+        T A0213 = m[1][0] * m[3][2] - m[1][2] * m[3][0];
+        T A0312 = m[1][0] * m[2][3] - m[1][3] * m[2][0];
+        T A0212 = m[1][0] * m[2][2] - m[1][2] * m[2][0];
+        T A0113 = m[1][0] * m[3][1] - m[1][1] * m[3][0];
+        T A0112 = m[1][0] * m[2][1] - m[1][1] * m[2][0];
+
+        T det = m[0][0] * ( m[1][1] * A2323 - m[1][2] * A1323 + m[1][3] * A1223 )
+              - m[0][1] * ( m[1][0] * A2323 - m[1][2] * A0323 + m[1][3] * A0223 )
+              + m[0][2] * ( m[1][0] * A1323 - m[1][1] * A0323 + m[1][3] * A0123 )
+              - m[0][3] * ( m[1][0] * A1223 - m[1][1] * A0223 + m[1][2] * A0123 );
+
+        if (IsEqual<T>(det, 0))
+        {
+            throw std::logic_error("Uninvertible matrix!");
+        }
+
+        det = 1 / det;
+
+        im[0][0] = det *   ( m[1][1] * A2323 - m[1][2] * A1323 + m[1][3] * A1223 );
+        im[0][1] = det * - ( m[0][1] * A2323 - m[0][2] * A1323 + m[0][3] * A1223 );
+        im[0][2] = det *   ( m[0][1] * A2313 - m[0][2] * A1313 + m[0][3] * A1213 );
+        im[0][3] = det * - ( m[0][1] * A2312 - m[0][2] * A1312 + m[0][3] * A1212 );
+        im[1][0] = det * - ( m[1][0] * A2323 - m[1][2] * A0323 + m[1][3] * A0223 );
+        im[1][1] = det *   ( m[0][0] * A2323 - m[0][2] * A0323 + m[0][3] * A0223 );
+        im[1][2] = det * - ( m[0][0] * A2313 - m[0][2] * A0313 + m[0][3] * A0213 );
+        im[1][3] = det *   ( m[0][0] * A2312 - m[0][2] * A0312 + m[0][3] * A0212 );
+        im[2][0] = det *   ( m[1][0] * A1323 - m[1][1] * A0323 + m[1][3] * A0123 );
+        im[2][1] = det * - ( m[0][0] * A1323 - m[0][1] * A0323 + m[0][3] * A0123 );
+        im[2][2] = det *   ( m[0][0] * A1313 - m[0][1] * A0313 + m[0][3] * A0113 );
+        im[2][3] = det * - ( m[0][0] * A1312 - m[0][1] * A0312 + m[0][3] * A0112 );
+        im[3][0] = det * - ( m[1][0] * A1223 - m[1][1] * A0223 + m[1][2] * A0123 );
+        im[3][1] = det *   ( m[0][0] * A1223 - m[0][1] * A0223 + m[0][2] * A0123 );
+        im[3][2] = det * - ( m[0][0] * A1213 - m[0][1] * A0213 + m[0][2] * A0113 );
+        im[3][3] = det *   ( m[0][0] * A1212 - m[0][1] * A0212 + m[0][2] * A0112 );
+
+        return im;
     }
 
     template<typename T>
